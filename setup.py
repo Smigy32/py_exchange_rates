f84de39b78ffafe1fcf57232397af29c42fdb56c
Illia Smyhunov <ismigunoff@gmail.com>
2022-04-26 01:29:05 +0300

    Add matplotlib to install_requires

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description_content_type="text/markdown",
 
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=["requests", "matplotlib"],
 
     project_urls={
         "Source": "https://github.com/Smigy32/py_exchange_rates",
